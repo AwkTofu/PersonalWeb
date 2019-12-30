b3b93e77ec2977838817a3aa5c78e00de6cadb1a
fanzhong <fanzhongzeng78@gmail.com>
2019-12-30 04:03:27 -0500

    added character but can't migrate....issue with postgreSQL

---
+++ Backend/personalWebBackend/db/migrate/20191230090123_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :exp
+
+      t.timestamps
+    end
+  end
+end
