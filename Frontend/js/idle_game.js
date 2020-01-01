eee090582486e491ecd9ee98a6764ef372df870a
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-01-01 16:30:06 -0500

    added basic loop

--- Frontend/js/idle_game.js
+++ Frontend/js/idle_game.js
@@ -4,7 +4,24 @@ fetch("http://localhost:3000/characters")
 	.then(json => {
 		console.log(json)
 	})
-
+let player;
 
 let gamecanvas = document.getElementById("game");
-console.log(canvas)
+
+let createGameMainScreenDiv = (player) => {
+	let gameMainScreen = document.createElement("div");
+	gameMainScreen.id = "GameMainScreen"
+
+	gamecanvas.append(gameMainScreen);
+} 
+
+let mainGameLoop = () => {
+	console.log("tests")
+}
+
+function mainGame()
+{
+	createGameMainScreenDiv()
+	setInterval(mainGameLoop, 1000)
+}
+mainGame()
---
+++ Frontend/js/player.js
@@ -0,0 +1,5 @@
+class Player {
+    constructor(id, name, lv, exp) {
+        
+    }
+}
